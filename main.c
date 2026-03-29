04b7d032715de185bec55062cc324571dca8a0fe
Amgaa <enhamgalane361@gmail.com>
2026-03-29 12:06:30 +0800

    Add version 3: Developer B's contribution

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main() {
-    printf("Hello World!\n");
-    printf("This is version 2!\n");
-    return 0;
+printf("Hello World!\n");
+printf("This is version 2!\n");
+printf("Added by Developer B!\n");
+return 0;
 }
